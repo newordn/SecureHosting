bab2b0577b0e06c30aa0c95f6ae73a33ad01061d
Neword <newordn@gmail.com>
2019-07-26 22:36:53 +0100

    Initial commit

---
+++ src/main/java/com/neword/demo/SecureHostingApplication.java
@@ -0,0 +1,13 @@
+package com.neword.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureHostingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureHostingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neword/demo/SecureHostingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.neword.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecureHostingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
